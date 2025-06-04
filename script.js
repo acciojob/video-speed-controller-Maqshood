20d8540027934a93b7c4c5f7f44875f494d6123e
SK Maqshood <skmaqshood99@gmail.com>
2025-06-04 13:27:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,52 @@
-const inputs = document.querySelectorAll('.controls input');
+const video = document.querySelector('.player__video');
+const toggle = document.querySelector('.toggle');
+const rewind = document.querySelector('.rewind');
+const fastforward = document.querySelector('.fastforward');
+const volume = document.querySelector('input[name="volume"]');
+const playbackSpeed = document.querySelector('input[name="playbackRate"]');
+const progress = document.querySelector('.progress');
+const progressFilled = document.querySelector('.progress__filled');
 
-    function handleUpdate() {
-      const suffix = this.dataset.sizing || '';
-      document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    }
+// Toggle play/pause
+function togglePlay() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+  updateButton();
+}
 
-    inputs.forEach(input => input.addEventListener('change', handleUpdate));
-    inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+function updateButton() {
+  toggle.textContent = video.paused ? '►' : '❚ ❚';
+}
+
+// Update volume and speed
+volume.addEventListener('input', () => {
+  video.volume = volume.value;
+});
+
+playbackSpeed.addEventListener('input', () => {
+  video.playbackRate = playbackSpeed.value;
+});
+
+// Skip forward/backward
+rewind.addEventListener('click', () => {
+  video.currentTime -= 10;
+});
+
+fastforward.addEventListener('click', () => {
+  video.currentTime += 25;
+});
+
+// Update progress bar
+video.addEventListener('timeupdate', () => {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressFilled.style.flexBasis = ${percent}%;
+});
+
+// Toggle play with button
+toggle.addEventListener('click', togglePlay);
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
